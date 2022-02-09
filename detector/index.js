df6b5846e2bd2cb829c1899e0e7aa200351f17af
wawachuchu <60635447+gtopacio@users.noreply.github.com>
2022-02-09 19:50:27 +0800

    Central Handler

--- detector/index.js
+++ detector/index.js
@@ -4,53 +4,61 @@
 */
 
 const ZongJi = require('@vlasky/zongji');
-const kafkaProducer = require('../lib/kafka/producer');
+// const kafkaProducer = require('../lib/kafka/producer');
 const formulateChange = require('../lib/formulateChange');
+
+const handler = require('../lib/handler/centralHandler');
 
 require('dotenv').config();
 const changeTopicName = process.env.CHANGE_TOPIC_NAME;
 let timeOut;
 
-let zongji = new ZongJi({ 
-    user: process.env.REPLICATOR_USERNAME,
-    password: process.env.REPLICATOR_PASSWORD,
-    host: process.env.NODE_HOSTNAME
- });
+let zongji;
 
-zongji.on('binlog', async function(evt) {
+function binlogHandler(evt) {
     let changeData = formulateChange({evt});
+    if(!changeData)
+        return;
     console.log("Bin Log", changeData);
-    kafkaProducer.publishChange({topic: changeTopicName, value: JSON.stringify(changeData)});
-});
+    handler(changeData);
+    // kafkaProducer.publishChange({topic: changeTopicName, value: JSON.stringify(changeData)});
+}
 
-zongji.on('error', retryConnection);
-
-function retryConnection(){
-    console.log("Detector Error, restarting connection...");
-    zongji.stop();
-    timeOut = setTimeout(start, 1000);
+function retryConnection(e){
+    if(e.code === "PROTOCOL_CONNECTION_LOST" || e.code === "ECONNREFUSED"){
+        console.log({message: "Detector Error, restarting connection...", code: e.code});
+        if(zongji)
+            zongji.stop();
+        timeOut = setTimeout(start, 1000);
+    }
 }
 
 function start(){
     if(timeOut){
         clearTimeout(timeOut);
     }
-    console.log("Detector is starting...")
-    try{
-        zongji.start({
-            includeEvents: ['tablemap','writerows', 'updaterows', 'deleterows'],
-            startAtEnd: true
-        });
-        console.log("Detector started");
-    }
-    catch(e){
-        console.error(e);
-    }
+    console.log("Detector is starting...");
+    let zongji = new ZongJi({ 
+        user: process.env.REPLICATOR_USERNAME,
+        password: process.env.REPLICATOR_PASSWORD,
+        host: process.env.NODE_HOSTNAME,
+        port: process.env.MYSQL_PORT,
+        serverId: 98
+     });
+    zongji.on('binlog', binlogHandler);
+    zongji.on('error', retryConnection);
+    zongji.on('ready', () => console.log("Detector started"));
+    zongji.start({
+        includeEvents: ['tablemap','writerows', 'updaterows', 'deleterows'],
+        startAtEnd: true
+    });
 }
 
 function stop(){
     clearTimeout(timeOut);
-    zongji.stop();
+    if(zongji){
+        zongji.stop();
+    }
 }
 
 module.exports = { start, stop }
--- testing.js
+++ testing.js
@@ -1,7 +1,16 @@
-const detector = require('./detector/index');
+const handler = require('./lib/handler/centralHandler');
 
-detector.start();
+let insert1 = {
+    origin: "CENTRAL",
+    timestamp: 1644396489,
+    type: "update",
+    rows: [ {
+        before: { id: 412330, name: 'TESTING 1', year: 2000, rank: null, lastUpdated: 1644396481 },
+        after: { id: 412330, name: 'TESTING 1', year: 2020, rank: 9, lastUpdated: 1644396489 }
+    } ]
+}
 
-process.on('SIGINT', () => {
-    detector.stop();
-})
+handler(insert1).then(() => {
+    process.exit(0);
+});
+
