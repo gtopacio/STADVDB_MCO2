e409ff2f2a85806e9595e707475feac74898cd84
wawachuchu <60635447+gtopacio@users.noreply.github.com>
2022-02-12 01:43:37 +0800

    More Replicator Bugs

--- replicator/index.js
+++ replicator/index.js
@@ -12,20 +12,23 @@ async function start(){
     await consumer.connect();
 }
 
-async function subscribe(topic, eachMessage){
+async function subscribe(topic){
     try{
         await consumer.subscribe({topic, fromBeginning: true});
-        await consumer.run({eachMessage});
     }
     catch(e){
         console.error(e);
     }
 }
 
+async function run(eachMessage){
+    await consumer.run({eachMessage});
+}
+
 async function stop(){
     await consumer.disconnect();
 }
 
-module.exports = { start, stop, subscribe }
+module.exports = { start, stop, subscribe, run }
 
 
