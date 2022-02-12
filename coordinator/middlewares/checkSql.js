73937a94234e026ab18d07ea68bb4b5b77eed6d9
RJ-Csc <rolan_cosico@dlsu.edu.ph>
2022-02-12 21:39:55 +0800

    checkers

--- coordinator/middlewares/checkSql.js
+++ coordinator/middlewares/checkSql.js
@@ -2,42 +2,19 @@ const { Parser } = require('node-sql-parser');
 const parser = new Parser();
 
 async function checkSql (req, res, next) {
+  let queries = req.body.queries;
+  console.log(queries);
 
-  let trans = req.body.trans;
-  let queries;
-  if (trans == "single") {
-    queries = req.body.queries;
-    console.log(queries);
-
-    for (let i = 0; i < queries.length; i++) {
-      try {
-        const ast = parser.astify(queries[i]);
-      } catch(e) {
-        console.error(e);
-        res.send("INVALID SQL");
-        return;
-      }
+  for (let i = 0; i < queries.length; i++) {
+    try {
+      const ast = parser.astify(queries[i]);
+    } catch(e) {
+      console.error(e);
+      res.send("INVALID SQL");
+      return;
     }
-    next();
   }
-  else {
-    queryCollection = req.body.queryCollection;
-    console.log(queryCollection);
-
-    for (let i = 0; i < queryCollection.length; i++) {
-      queries = queryCollection[i];
-      for (let j = 0; j < queries.length; j++) {
-        try {
-          const ast = parser.astify(queries[j]);
-        } catch(e) {
-          console.error(e);
-          res.send("INVALID SQL");
-          return;
-        }
-      }
-    }
-    next();
-  }
+  next();  
 }
 
 module.exports = checkSql;
