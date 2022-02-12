53b01b956151dcc2722f6d00ee6e101c7cbad2f6
RJ-Csc <rolan_cosico@dlsu.edu.ph>
2022-02-12 20:40:12 +0800

    Front end

--- coordinator/routes.js
+++ coordinator/routes.js
@@ -3,6 +3,7 @@ const controller = require ('./controllers/controller.js');
 const checkCentral = require('./middlewares/checkCentral.js');
 const app = express();
 app.set('views', __dirname+'/views');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', controller.getIndex);
 app.post('/query', checkCentral, controller.query);
