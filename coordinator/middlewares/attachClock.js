81dc97ac6a3048b120115cdd2b39e8830dbd7ff2
wawachuchu <60635447+gtopacio@users.noreply.github.com>
2022-02-14 09:44:18 +0800

    Initial Vector

---
+++ coordinator/middlewares/attachClock.js
@@ -0,0 +1,5 @@
+function attachClock(req, res, next){
+    next();
+}
+
+module.exports = attachClock;
--- coordinator/routes.js
+++ coordinator/routes.js
@@ -3,12 +3,13 @@ const controller = require ('./controllers/controller.js');
 const checkCentral = require('./middlewares/checkCentral.js');
 const checkSql = require('./middlewares/checkSql.js');
 const checkReadOnly = require('./middlewares/checkReadOnly.js');
+const attachClock = require('./middlewares/attachClock');
 const app = express();
 app.set('views', __dirname+'/views');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', controller.getIndex);
-app.post('/query', checkSql, checkReadOnly, checkCentral, controller.query);
+app.post('/query', checkCentral, checkSql, checkReadOnly, attachClock, controller.query);
 app.get("/ping", controller.ping);
 app.get("/urls", controller.urls)
 
