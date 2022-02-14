d433d7c60e557911384fa8579f1cdebb5db34bb3
wawachuchu <60635447+gtopacio@users.noreply.github.com>
2022-02-15 07:42:21 +0800

    m

--- rebalancer/index.js
+++ rebalancer/index.js
@@ -64,7 +64,7 @@ async function start(){
                     console.log("ATTEMPT", record);
                     let receivedClock = {clock: {CENTRAL: record.CENTRAL, L1980: record.L1980, GE1980: record.GE1980}};
                     await Promise.all([connL1980.beginTransaction(), connGE1980.beginTransaction()]);
-                    let [storedRecordL1980] = connL1980.execute("SELECT CENTRAL, L1980, GE1980, tombstone FROM movies WHERE id = ? FOR UPDATE", [row.after.id]);
+                    let [storedRecordL1980] = await connL1980.execute("SELECT CENTRAL, L1980, GE1980, tombstone FROM movies WHERE id = ? FOR UPDATE", [record.id]);
                     storedRecordL1980 = storedRecordL1980[0];
                     recordL1980 = {clock: storedRecordL1980};
                     if(vectorClock.isIdentical(recordL1980, receivedClock)){
@@ -80,7 +80,7 @@ async function start(){
                         });
                     }
 
-                    let [storedRecordGE1980] = await connGE1980.execute("SELECT CENTRAL, L1980, GE1980, tombstone FROM movies WHERE id = ? FOR UPDATE");
+                    let [storedRecordGE1980] = await connGE1980.execute("SELECT CENTRAL, L1980, GE1980, tombstone FROM movies WHERE id = ? FOR UPDATE", [record.id]);
                     storedRecordGE1980 = storedRecordGE1980[0];
                     recordGE1980 = {clock: storedRecordGE1980};
 
@@ -111,7 +111,7 @@ async function start(){
                 if(origin == "GE1980" && record.year < 1980 && record.tombstone == 0){
                     let receivedClock = {clock: {CENTRAL: record.CENTRAL, L1980: record.L1980, GE1980: record.GE1980}};
                     await Promise.all([connL1980.beginTransaction(), connGE1980.beginTransaction()]);
-                    let [storedRecordGE1980] = connGE1980.execute("SELECT CENTRAL, L1980, GE1980, tombstone FROM movie WHERE id = ? FOR UPDATE", [row.after.id]);
+                    let [storedRecordGE1980] = connGE1980.execute("SELECT CENTRAL, L1980, GE1980, tombstone FROM movie WHERE id = ? FOR UPDATE", [record.id]);
                     storedRecordGE1980 = storedRecordGE1980[0];
                     recordGE1980 = {clock: storedRecordGE1980};
                     if(vectorClock.isIdentical(recordGE1980, receivedClock)){
@@ -127,7 +127,7 @@ async function start(){
                         });
                     }
 
-                    let [storedRecordL1980] = await connL1980.execute("SELECT CENTRAL, L1980, GE1980, tombstone FROM movie WHERE id = ? FOR UPDATE");
+                    let [storedRecordL1980] = await connL1980.execute("SELECT CENTRAL, L1980, GE1980, tombstone FROM movie WHERE id = ? FOR UPDATE", [record.id]);
                     storedRecordL1980 = storedRecordL1980[0];
                     recordL1980 = {clock: storedRecordL1980};
 
